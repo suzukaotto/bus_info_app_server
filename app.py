b6e78038780d6eb59f135b3261bbeb392a51a8ac
suzukaotto <me@szk.kr>
2024-10-07 10:13:42 +0000

    update: create flask app

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from dotenv import load_dotenv
 
 # load setting
 load_dotenv()
+SERVICE_KEY = os.getenv('SERVICE_KEY')
 with open('./config.json', 'r') as f:
     CONFIG = json.load(f)
 
-SERVICE_KEY = os.getenv('SERVICE_KEY')
-
+# create app
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return "Hello World"
 
+# start app
 if __name__ == '__main__':
     app.run(CONFIG['HOST'], CONFIG['PORT'], debug=True)
